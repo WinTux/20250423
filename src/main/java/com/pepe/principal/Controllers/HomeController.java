55c14a71e892005d868717d97d2c1e687ee7a6d3
Rusok <rusok.software@gmail.com>
2025-04-23 19:26:08 -0400

    Shutdown

--- src/main/java/com/pepe/principal/Controllers/HomeController.java
+++ src/main/java/com/pepe/principal/Controllers/HomeController.java
@@ -1,16 +1,26 @@
 package com.pepe.principal.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value="/home") //http://localhost:8080/home
 public class HomeController {
+	@Autowired
+	private ApplicationContext context;
 	//@RequestMapping(method=RequestMethod.GET)
 	@GetMapping
 	public String index() {
 		return "home/pagina";
+	}
+	@PostMapping
+	public void apagar() {
+		SpringApplication.exit(context);
 	}
 }
--- src/main/java/com/pepe/principal/Controllers/PrimerControlador.java
+++ src/main/java/com/pepe/principal/Controllers/PrimerControlador.java
@@ -1,16 +1,29 @@
 package com.pepe.principal.Controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PrimerControlador {
+	@Value("${valor.secreto}")
+	private String val01;
+	@Value("${edad}")
+	private int val02;
+	@Value("${otro.valor}")
+	private String val03;
 	
 	// Endpoint
 	@RequestMapping("/") // http://localhost:8080/
 	@ResponseBody
 	public String Saludo() {
 		return "Hola a todos";
+	}
+	@RequestMapping("/props") // http://localhost:8080/props
+	@ResponseBody
+	public String Propiedades() {
+		return val01+" "+val02+" "+val03;
+		
 	}
 }
