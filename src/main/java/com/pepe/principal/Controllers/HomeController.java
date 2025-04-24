b9d61bb5b5e0e469d9aeb7d8600461b4f3613410
Rusok <rusok.software@gmail.com>
2025-04-24 19:23:43 -0400

    Logs

--- src/main/java/com/pepe/principal/Controllers/HomeController.java
+++ src/main/java/com/pepe/principal/Controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.pepe.principal.Controllers;
 
+import java.util.logging.Logger;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	@Autowired
 	private ApplicationContext context;
+	
+	private static final Logger logger = Logger.getLogger(HomeController.class.getName());
+	
 	//@RequestMapping(method=RequestMethod.GET)
 	@GetMapping
 	public String index() {
@@ -21,6 +26,15 @@ public class HomeController {
 	}
 	@PostMapping
 	public void apagar() {
+		
+		logger.severe("Este es un mensaje grave");
+		logger.warning("Esta es una simple advertencia");
+		logger.info("Se procede a finalizar el servicio (ordenado por el usuario mediante context)");
+		logger.config("Mensaje de configurción");
+		logger.fine("Mensaje fino de depurción");
+		logger.finer("Mensaje más fino");
+		logger.finest("EL mensaje más fino de depuración");
+		
 		SpringApplication.exit(context);
 	}
 }
--- src/main/java/com/pepe/principal/Controllers/PrimerControlador.java
+++ src/main/java/com/pepe/principal/Controllers/PrimerControlador.java
@@ -1,5 +1,7 @@
 package com.pepe.principal.Controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +16,19 @@ public class PrimerControlador {
 	@Value("${otro.valor}")
 	private String val03;
 	
+	private static final Logger logger = LoggerFactory.getLogger(PrimerControlador.class);
+	
 	// Endpoint
 	@RequestMapping("/") // http://localhost:8080/
 	@ResponseBody
 	public String Saludo() {
+		logger.info("Llegó exitosamente a endpoint /");
 		return "Hola a todos";
 	}
 	@RequestMapping("/props") // http://localhost:8080/props
 	@ResponseBody
 	public String Propiedades() {
+		logger.error("Este no es un error, es una prueba e log.");
 		return val01+" "+val02+" "+val03;
 		
 	}
