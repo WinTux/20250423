b046513200f1dc3c9e39ea7d864fddc23e643674
Rusok <rusok.software@gmail.com>
2025-05-02 19:34:21 -0400

    cors

--- src/main/java/com/pepe/principal/Configurations/ConfiguracionDeSeguridad.java
+++ src/main/java/com/pepe/principal/Configurations/ConfiguracionDeSeguridad.java
@@ -1,17 +1,21 @@
 package com.pepe.principal.Configurations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.pepe.principal.Filter.ControlConexionFilter;
 
 import jakarta.servlet.Filter;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class ConfiguracionDeSeguridad {
 	@Autowired
 	ControlConexionFilter filtro;
@@ -24,4 +28,15 @@ public class ConfiguracionDeSeguridad {
 		f.setOrder(Ordered.HIGHEST_PRECEDENCE);
 		return f;
 	};
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+		return http
+				.securityMatcher(EndpointRequest.toAnyEndpoint())
+				.authorizeHttpRequests(
+						authorize -> 
+						authorize
+						  .anyRequest()
+						  .permitAll())
+				.build();
+	}
 }
